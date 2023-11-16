63ed0d0e553f4c6e489861336d6df39ad833415f
jhjang7408 <jhjang7408@naver.com>
2023-11-16 18:37:11 +0900

    Authentication 2가지 타입

--- src/main/java/com/temisone/jwtsecurity/config/auth/PrincipalDetails.java
+++ src/main/java/com/temisone/jwtsecurity/config/auth/PrincipalDetails.java
@@ -1,19 +1,25 @@
 package com.temisone.jwtsecurity.config.auth;
 
 import com.temisone.jwtsecurity.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
 
     public PrincipalDetails(User user){
         this.user = user;
     }
+
+
 
 
     // 해당 User의 권한을 리턴하는 곳
@@ -57,5 +63,19 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
--- src/main/java/com/temisone/jwtsecurity/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/temisone/jwtsecurity/config/oauth/PrincipalOauth2UserService.java
@@ -13,13 +13,19 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
 
+        // registrationId로 어떤 OAuth로 로그인 했는지 확인가능
         System.out.println("getClientRegistration : " + userRequest.getClientRegistration());
+
+
         System.out.println("getAccessToken : " + userRequest.getAccessToken().getTokenValue());
+
+        // 구글로그인 버튼 클릭 -> 구글로그인창 -> 로그인 완료 -> code를 리턴(OAuth-Client 라이브러리) -> AccessToken 요청
+        // userRequest 정보 -> loadUser 함수 호출 -> 구글로부터 회원프로필 받아줌
         System.out.println("getAttributes : " + super.loadUser(userRequest).getAttributes());
 
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
         // 회원가입을 강제로 진행해볼 예정
-
-
         return super.loadUser(userRequest);
     }
 
